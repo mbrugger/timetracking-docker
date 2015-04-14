798392f6914c04e1c6f62dd057559f35cb6f9125
Martin Brugger <martin@brugger.eu>
2015-04-14 11:36:15 +0200

    removed unused code (assets:precompile in container startup moved to docker build)

--- prepare_env.sh
+++ prepare_env.sh
@@ -2,5 +2,3 @@
 
 echo "rake db:create db:migrate"
 sudo -u app -E bash -c 'bundle exec rake db:create db:migrate'
-#echo "rake assets:precompile"
-#sudo -u app -E bash -c 'bundle exec rake assets:precompile'
